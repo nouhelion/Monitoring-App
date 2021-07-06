75a15854eaecdd1d0d6c4307b3eac8e839bbdbec
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2021-07-06 20:14:26 +0100

    secong page
    still needs some work

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Connexion</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 
 </head>
+
 <body>
 	<div class="limiter">
 		<div class="container-login100">
@@ -28,14 +29,14 @@
 				<form class="login100-form validate-form" action="" method="POST">
 					<div class="wrap-input100 validate-input m-b-26" data-validate="Username is required">
 						<span class="label-input100">Nom d'Utilisateur</span>
-                        <i class="fa fa-user"></i>
+
 						<input class="input100" type="text" name="username" placeholder="Entrer votre Nom d'Utilisateur">
 						<span class="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input m-b-18" data-validate = "Password is required">
+					<div class="wrap-input100 validate-input m-b-18" data-validate="Password is required">
 						<span class="label-input100">Mot de passe</span>
-						<i class="fa fa-lock"></i>
+
 						<input class="input100" type="password" name="passwo_log" placeholder="Entrer votre Mot de passe">
 						<span class="focus-input100"></span>
 					</div>
@@ -47,47 +48,48 @@
 			</div>
 		</div>
 	</div>
-	
-<!--===============================================================================================-->
+
+	<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/daterangepicker/moment.min.js"></script>
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
-	<?php 
-    require('db.php');
-    session_start();
-    if(isset($_POST['username'])){
-        $username = stripslashes($_REQUEST['username']);
-        $username = mysqli_real_escape_string($link,$username);
-        $password = stripslashes($_REQUEST['passwo_log']);
-        $password = mysqli_real_escape_string($link,$password);
-        $query = "SELECT * FROM `admins` WHERE username='$username' and password='$password'";
-        $result = mysqli_query($link, $query) ;
-        $rows = mysqli_num_rows($result);
-        if($rows==1){
-            $_SESSION['username'] = $username;
-            header("Location: result.php");
-        } else{
-            echo "<div id='loginpage'>
+	<?php
+	require('db.php');
+	session_start();
+	if (isset($_POST['username'])) {
+		$username = stripslashes($_REQUEST['username']);
+		$username = mysqli_real_escape_string($link, $username);
+		$password = stripslashes($_REQUEST['passwo_log']);
+		$password = mysqli_real_escape_string($link, $password);
+		$query = "SELECT * FROM `admins` WHERE username='$username' and password='$password'";
+		$result = mysqli_query($link, $query);
+		$rows = mysqli_num_rows($result);
+		if ($rows == 1) {
+			$_SESSION['username'] = $username;
+			header("Location: page.php");
+		} else {
+			echo "<div id='loginpage'>
             <p>Username/password is incorrect.</p>";
-        }
-    }
-    ?>
-	 <script>
-if ( window.history.replaceState ) {
-  window.history.replaceState( null, null, window.location.href );
-}
-</script>
+		}
+	}
+	?>
+	<script>
+		if (window.history.replaceState) {
+			window.history.replaceState(null, null, window.location.href);
+		}
+	</script>
 </body>
+
 </html>
---
+++ page.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!--
+Gestion des autorisation
+http://www.templatemo.com/tm-486-new-event
+-->
+<title>Ministere des mines, de l'energie et de l'environnement</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/owl.theme.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style.css">
+
+<!-- Google Font -->
+<link href='https://fonts.googleapis.com/css?family=Poppins:400,500,600' rel='stylesheet' type='text/css'>
+
+</head>
+<body data-spy="scroll" data-offset="50" data-target=".navbar-collapse">
+
+<!-- =========================
+     PRE LOADER       
+============================== -->
+<div class="preloader">
+
+	<div class="sk-rotating-plane"></div>
+
+</div>
+
+
+<!-- =========================
+     NAVIGATION LINKS     
+============================== -->
+<div class="navbar navbar-fixed-top custom-navbar" role="navigation">
+	<div class="container">
+
+		<!-- navbar header -->
+		<div class="navbar-header">
+			<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+				<span class="icon icon-bar"></span>
+			</button>
+			<img src="images/roy.png" > 
+		</div>
+
+		<div class="collapse navbar-collapse">
+
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#intro" class="smoothScroll">Intro</a></li>
+				<li><a href="#overview" class="smoothScroll">choix</a></li>
+				<li><a href="#speakers" class="smoothScroll">Speakers</a></li>
+				<li><a href="#program" class="smoothScroll">Programs</a></li>
+				<li><a href="#register" class="smoothScroll">Register</a></li>
+				<li><a href="#venue" class="smoothScroll">Venue</a></li>
+				<li><a href="#sponsors" class="smoothScroll">Sponsors</a></li>
+				<li><a href="#contact" class="smoothScroll">Contact</a></li>
+			</ul>
+
+		</div>
+
+	</div>
+</div>
+
+
+<!-- =========================
+    INTRO SECTION   
+============================== -->
+<section id="intro" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<h1 class="wow fadeInUp" data-wow-delay="1.6s">Gestion des autorisations</h1>
+				<a href="#overview" class="btn btn-lg btn-default smoothScroll wow fadeInUp hidden-xs" data-wow-delay="2.3s">Energie solaire</a>
+				<a href="#Register" class="btn btn-lg btn-default smoothScroll wow fadeInUp hidden-xs" data-wow-delay="2.3s">Energie Eolienne</a>
+				<a href="#third" class="btn btn-lg btn-default smoothScroll wow fadeInUp hidden-xs" data-wow-delay="2.3s">Energie Hydroelectrique</a>
+			</div>
+
+
+		</div>
+	</div>
+</section>
+
+
+<!-- =========================
+   REGISTER SECTION   
+============================== -->
+<section id="register" class="parallax-section">
+	<div class="container">
+		<div class="row">
+
+			<div class="wow fadeInUp col-md-7 col-sm-7" data-wow-delay="0.6s">
+				<h2>Direction des Energies Renouvelables et de l'Efficacité Energétique			</h2>
+				<h3>Dossiers de demande d'autorisation provisoire pour le développement des projets EnR dans le cadre de la loi 13-09</h3>
+			</div>
+
+			<div class="wow fadeInUp col-md-5 col-sm-5" data-wow-delay="1s">
+				<form action="#" method="post">
+					<input name="firstname" type="text" class="form-control" id="firstname" placeholder="First Name">
+					<input name="lastname" type="text" class="form-control" id="lastname" placeholder="Last Name">
+					<input name="phone" type="telephone" class="form-control" id="phone" placeholder="Phone Number">
+					<input name="email" type="email" class="form-control" id="email" placeholder="Email Address">
+					<div class="col-md-offset-6 col-md-6 col-sm-offset-1 col-sm-10">
+						<input name="submit" type="submit" class="form-control" id="submit" value="REGISTER">
+					</div>
+				</form>
+			</div>
+
+			<div class="col-md-1"></div>
+
+		</div>
+	</div>
+</section>
+
+
+
+
+
+
+
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+
+<!-- =========================
+     SCRIPTS   
+============================== -->
+<script src="js_/jquery.js"></script>
+<script src="js_/bootstrap.min.js"></script>
+<script src="js_/jquery.parallax.js"></script>
+<script src="js_/owl.carousel.min.js"></script>
+<script src="js_/smoothscroll.js"></script>
+<script src="js_/wow.min.js"></script>
+<script src="js_/custom.js"></script>
+
+</body>
+</html>
