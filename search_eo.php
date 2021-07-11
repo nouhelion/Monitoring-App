6e735334de621be08bd58c15bfc4b30887d96325
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2021-07-11 20:53:22 +0100

    do table stuff
    eoliene

---
+++ search_eo.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!--
+Gestion des autorisation
+http://www.templatemo.com/tm-486-new-event
+-->
+	<title>Ministere des mines, de l'energie et de l'environnement</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/owl.theme.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<!-- Main Style Css -->
+
+	<!-- Main css -->
+	<link rel="stylesheet" href="css/ele.css">
+	<link rel="stylesheet" href="css/table_eo.css">
+
+	<!-- Google Font -->
+	<link href='https://fonts.googleapis.com/css?family=Poppins:400,500,600' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body data-spy="scroll" data-offset="50" data-target=".navbar-collapse" class="form-v10">
+
+	<!-- =========================
+     PRE LOADER       
+============================== -->
+	<div class="preloader">
+
+		<div class="sk-rotating-plane"></div>
+
+	</div>
+
+
+	<!-- =========================
+     NAVIGATION LINKS     
+============================== -->
+
+	<div class="navbar navbar-fixed-top custom-navbar" role="navigation">
+		<div class="container">
+
+			<!-- navbar header -->
+			<div class="navbar-header">
+				<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon icon-bar"></span>
+					<span class="icon icon-bar"></span>
+					<span class="icon icon-bar"></span>
+				</button>
+				<img src="images/roy.png">
+			</div>
+
+			<div class="collapse navbar-collapse">
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="home.php" class="smoothScroll">Home</a></li>
+					<li><a href="energieEolienne.php" class="smoothScroll">Ajouter Un Projet</a></li>
+					<li><a href="#search" class="smoothScroll">Rechercher</a></li>
+					<li><a href="#speakers" class="smoothScroll">Alertes</a></li>
+					<li><a href="#speakers" class="smoothScroll">Reporting</a></li>
+				</ul>
+
+			</div>
+
+		</div>
+	</div>
+
+
+	<!-- =========================
+    INTRO SECTION   
+============================== -->
+	<section id="intro" class="parallax-section">
+		<div class="container">
+			<div class="row">
+
+				<div class="wow fadeInUp col-md-7 col-sm-7" data-wow-delay="0.6s">
+					<h2 style="color: black;">Direction des Energies Renouvelables et de l'energie eolienne</h2>
+					<h4 style="color: white;">Dossiers de demande d'autorisation provisoire pour le développement des projets EnR dans le cadre de la loi 13-09</h4>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+
+	<!-- =========================
+   REGISTER SECTION   
+============================== -->
+
+
+
+	<!-- =========================
+    PROGRAM SECTION   
+============================== -->
+	<br><br><br><br>
+	<section id="search" class="parallax-section">
+
+		<div style="overflow-x:auto;">
+			<table>
+				<tr>
+					<th class="cell100 column1">Numero</th>
+					<th class="cell100 column2">Developpeur</th>
+					<th class="cell100 column3">Nom du projet</th>
+					<th class="cell100 column4">Région</th>
+					<th class="cell100 column5">Province</th>
+					<th class="cell100 column1">Localité</th>
+					<th class="cell100 column2">Dépôt au MEME</th>
+					<th class="cell100 column3">Envoi du dossier à l'ONEE</th>
+					<th class="cell100 column4">Etat D'avancement</th>
+					<th class="cell100 column5">Investissement du projet (MDH)</th>
+				</tr>
+				<tbody>
+				<tr>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>$100</td>
+				</tr>
+				<tr>
+				<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>$100</td>
+				</tr>
+				<tr>
+				<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>$100</td>
+				</tr>
+				<tr>
+				<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>$100</td>
+				</tr>
+				<tr>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>$100</td>
+				</tr>
+				<tr>
+				<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>$100</td>
+				</tr>
+				<tr>
+				<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>$100</td>
+				</tr>
+				<tr>
+				<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>Peter</td>
+					<td>Griffin</td>
+					<td>$100</td>
+					<td>$100</td>
+				</tr>
+				
+				</tbody>
+			</table>
+
+		</div>
+
+	</section>
+
+
+
+
+
+
+
+
+
+
+	<!-- Back top -->
+	<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+
+	<!-- =========================
+     SCRIPTS   
+============================== -->
+	<script src="js_/jquery.js"></script>
+	<script src="js_/bootstrap.min.js"></script>
+	<script src="js_/jquery.parallax.js"></script>
+	<script src="js_/owl.carousel.min.js"></script>
+	<script src="js_/smoothscroll.js"></script>
+	<script src="js_/wow.min.js"></script>
+	<script src="js_/custom.js"></script>
+	<script>
+		if (window.history.replaceState) {
+			window.history.replaceState(null, null, window.location.href);
+		}
+	</script>
+</body>
+
+</html>
