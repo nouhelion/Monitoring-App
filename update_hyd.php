ed92bb8b0e27a1fcebeb046f7e1faaddd97acbae
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2021-07-17 10:56:23 +0100

    Update update_hyd.php

--- update_hyd.php
+++ update_hyd.php
@@ -568,15 +568,18 @@ require('db.php');
                $("#add tr th:nth-last-child(9)").after("<th>Réponse ONEE/GRD Concerné en Cas Du "+iter+" Complément</th>");
                $("#add tr th:nth-last-child(9)").after("<th>Dépôt du "+iter+" Complément du Dossier Par le Développeur</th>");
                $("#add tr th:nth-last-child(9)").after("<th>Envoi du "+iter+" Complément du Dossier à l'ONEE/GRD Concerné</th>");  
-               $("#add tr th:nth-last-child(12)").after("<th>Réponse du Département de l'Eau en Cas Du "+iter+" Complément</th>");
-               //$("#add tr th:nth-last-child(9)").after("<th>Dépôt du "+iter+" Complément du Dossier Par le Développeur/ABH</th>");
-               /*$("#add tr th:nth-last-child(9)").after("<th>Envoi du "+iter+" Complément du Dossier à l'ABH</th>");*/
+               $("#add tr th:nth-last-child(4)").after("<th>Réponse du Département de l'Eau en Cas Du "+iter+" Complément</th>");
+               $("#add tr th:nth-last-child(4)").after("<th>Dépôt du "+iter+" Complément du Dossier Par le Développeur/ABH</th>");
+              $("#add tr th:nth-last-child(4)").after("<th>Envoi du "+iter+" Complément du Dossier à l'ABH</th>");
             }else{
                 if(iter ===2)
                   {
                     test1="<?php echo $Reponse2ONEE; ?>";
                    test2="<?php echo $Depotdu2complementdudossierparledeveloppeur; ?>";
                    test3="<?php echo $Envoidu2complementdudossieraONEE; ?>";
+                   test4="<?php echo $Reponse2Departement; ?>";
+                   test5="<?php echo $Depotdu2ABH; ?>";
+                   test6="<?php echo $Envoidu2ABH; ?>";
                    
                   }
                 else if(iter ===3)
@@ -584,21 +587,25 @@ require('db.php');
                     test1="<?php echo $Reponse3ONEE; ?>";
                    test2="<?php echo $Depotdu3complementdudossierparledeveloppeur; ?>";
                    test3="<?php echo $Envoidu3complementdudossieraONEE; ?>";
-                  
+                   test4="<?php echo $Reponse3Departement; ?>";
+                   test5="<?php echo $Depotdu3ABH; ?>";
+                   test6="<?php echo $Envoidu3ABH; ?>";
                   }
                   else if(iter ===4)
                   {
                     test1="<?php echo $Reponse4ONEE; ?>";
                    test2="<?php echo $Depotdu4complementdudossierparledeveloppeur; ?>";
                    test3="<?php echo $Envoidu4complementdudossieraONEE; ?>";
-                  
+                   test4="<?php echo $Reponse4Departement; ?>";
+                   test5="<?php echo $Depotdu4ABH; ?>";
+                   test6="<?php echo $Envoidu4ABH; ?>";
                   }
                 $("#add tr td:nth-last-child(9)").after('<td><input name="Reponse'+iter+'ONEE" type="text" id="Reponse'+iter+'ONEE" value="'+test1+'" ></td>');
                 $("#add tr td:nth-last-child(9)").after('<td><input name="Depotdu'+iter+'" type="text" id="Envoi du complement du dossier a ONEE" value="'+test2+'" ></td>');
                 $("#add tr td:nth-last-child(9)").after('<td><input name="Envoidu'+iter+'" type="text" id="Envoidu'+iter+'complementdudossieraONEE" value="'+test3+'" ></td>'); 
-                $("#add tr td:nth-last-child(12)").after('<td><input name="Reponse'+iter+'Departement" type="text" id="Reponse'+iter+'ONEE" value="'+test4+'" ></td>');
-               // $("#add tr td:nth-last-child(9)").after('<td><input name="Depotdu'+iter+'ABH" type="text" id="Envoi'+iter+'du complement du dossier a ONEE" value="'+test5+'" ></td>');
-                //$("#add tr td:nth-last-child(9)").after('<td><input name="Envoidu'+iter+'ABH" type="text" id="Envoidu'+iter+'complementdudossieraONEE" value="'+test6+'" ></td>');
+                $("#add tr td:nth-last-child(4)").after('<td><input name="Reponse'+iter+'Departement" type="text" id="Reponse'+iter+'ONEE" value="'+test4+'" ></td>');
+               $("#add tr td:nth-last-child(4)").after('<td><input name="Depotdu'+iter+'ABH" type="text" id="Envoi'+iter+'du complement du dossier a ONEE" value="'+test5+'" ></td>');
+                $("#add tr td:nth-last-child(4)").after('<td><input name="Envoidu'+iter+'ABH" type="text" id="Envoidu'+iter+'complementdudossieraONEE" value="'+test6+'" ></td>');
             } 
            
         });
