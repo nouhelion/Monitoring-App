dc5e210a9e1a523cd10d8acc57c84a8357b420e8
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2021-07-20 20:18:49 +0100

    meh

---
+++ diagram_eo/region_eo.php
@@ -0,0 +1,177 @@
+<?php
+
+require_once('db.php');
+if ($link) {
+    //FOR TTA
+    $TTA = "Tanger – Tétouan – Al Hoceima";
+    $sql = "SELECT Region, COUNT(*) as total
+    FROM energieeolienne
+     where Region='$TTA'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $TTA = $values['total'];
+    $sql = "UPDATE region 
+       SET 
+           nbr = '$TTA'
+       WHERE
+           nom = 'Tanger – Tétouan – Al Hoceima'";
+    mysqli_query($link, $sql);
+    //FOR O
+    $O = "Oriental";
+    $sql = "SELECT Region, COUNT(*) as total
+   FROM energieeolienne
+    where Region='$O'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $O = $values['total'];
+    $sql = "UPDATE region 
+      SET 
+          nbr = '$O'
+      WHERE
+          nom = 'Oriental'";
+    mysqli_query($link, $sql);
+    //FOR FM
+    $FM = "Fès - Meknès";
+    $sql = "SELECT Region, COUNT(*) as total
+   FROM energieeolienne
+    where Region='$FM'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $FM = $values['total'];
+    $sql = "UPDATE region 
+      SET 
+          nbr = '$FM'
+      WHERE
+          nom = 'Fès - Meknès'";
+    mysqli_query($link, $sql);
+    //FOR RSK
+    $RSK = "Rabat-Salé-Kénitra";
+    $sql = "SELECT Region, COUNT(*) as total
+   FROM energieeolienne
+    where Region='$RSK'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $RSK = $values['total'];
+    $sql = "UPDATE region 
+      SET 
+          nbr = '$RSK'
+      WHERE
+          nom = 'Rabat-Salé-Kénitra'";
+    mysqli_query($link, $sql);
+    //FOR BMK
+    $BMK = "Béni Mellal-Khénifra";
+    $sql = "SELECT Region, COUNT(*) as total
+   FROM energieeolienne
+    where Region='$BMK'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $BMK = $values['total'];
+    $sql = "UPDATE region 
+      SET 
+          nbr = '$BMK'
+      WHERE
+          nom = 'Béni Mellal-Khénifra'";
+    mysqli_query($link, $sql);
+    //FOR CS
+    $CS = "Casablanca-Settat";
+    $sql = "SELECT Region, COUNT(*) as total
+   FROM energieeolienne
+    where Region='$CS'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $CS = $values['total'];
+    $sql = "UPDATE region 
+      SET 
+          nbr = '$CS'
+      WHERE
+          nom = 'Casablanca-Settat'";
+    mysqli_query($link, $sql);
+    //FOR MS
+    $MS = "Marrakech-Safi";
+    $sql = "SELECT Region, COUNT(*) as total
+   FROM energieeolienne
+    where Region='$MS'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $MS = $values['total'];
+    $sql = "UPDATE region 
+      SET 
+          nbr = '$MS'
+      WHERE
+          nom = 'Marrakech-Safi'";
+    mysqli_query($link, $sql);
+    //FOR DT
+    $DT = "Drâa-Tafilalet";
+    $sql = "SELECT Region, COUNT(*) as total
+  FROM energieeolienne
+   where Region='$DT'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $DT = $values['total'];
+    $sql = "UPDATE region 
+     SET 
+         nbr = '$DT'
+     WHERE
+         nom = 'Drâa-Tafilalet'";
+    mysqli_query($link, $sql);
+    //FOR SM
+    $SM = "Souss-Massa";
+    $sql = "SELECT Region, COUNT(*) as total
+  FROM energieeolienne
+   where Region='$SM'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $SM = $values['total'];
+    $sql = "UPDATE region 
+     SET 
+         nbr = '$SM'
+     WHERE
+         nom = 'Souss-Massa'";
+    mysqli_query($link, $sql);
+    //FOR GON
+    $GON = "Guelmim-Oued Noun";
+    $sql = "SELECT Region, COUNT(*) as total
+  FROM energieeolienne
+   where Region='$GON'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $GON = $values['total'];
+    $sql = "UPDATE region 
+     SET 
+         nbr = '$GON'
+     WHERE
+         nom = 'Guelmim-Oued Noun'";
+    mysqli_query($link, $sql);
+    //FOR LSH
+    $LSH = "Laâyoune-Sakia El Hamra";
+    $sql = "SELECT Region, COUNT(*) as total
+  FROM energieeolienne
+   where Region='$LSH'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $LSH = $values['total'];
+    $sql = "UPDATE region 
+     SET 
+         nbr = '$LSH'
+     WHERE
+         nom = 'Laâyoune-Sakia El Hamra'";
+    mysqli_query($link, $sql);
+    //FOR DOD
+    $DOD = "Dakhla-Oued Ed Dahab";
+    $sql = "SELECT Region, COUNT(*) as total
+  FROM energieeolienne
+   where Region='$DOD'";
+    $result = mysqli_query($link, $sql);
+    $values = mysqli_fetch_assoc($result);
+    $DOD = $values['total'];
+    $sql = "UPDATE region 
+     SET 
+         nbr = '$DOD'
+     WHERE
+         nom = 'Dakhla-Oued Ed Dahab'";
+    mysqli_query($link, $sql);
+
+}
+
+
+?>
--- energieEolienne.php
+++ energieEolienne.php
@@ -227,7 +227,7 @@
                     <li><a href="#ajouter" class="smoothScroll">Ajouter Un Projet</a></li>
                     <li><a href="eo_search.php" class="smoothScroll">Rechercher</a></li>
                     <li><a href="#speakers" class="smoothScroll">Alertes</a></li>
-                    <li><a href="#speakers" class="smoothScroll">Reporting</a></li>
+                    <li><a href="report_eo.php" class="smoothScroll">Reporting</a></li>
                 </ul>
 
             </div>
