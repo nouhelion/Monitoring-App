d97becbe6a78817057de2f46b0f28fccfa460b42
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2021-07-24 02:17:26 +0100

    uwu yummy

--- EnergieHydro.php
+++ EnergieHydro.php
@@ -236,45 +236,36 @@ $result=mysqli_query($link,$sqli);
 while ($row = mysqli_fetch_assoc($result)) {
     $input=substr($row['Depot a la DEREE'], 0, 10);
     $date1=date('Y-m-d', strtotime($input. ' + 15 days'));
-    if($date1==$today or $date1<$today)
+    if($date1<=$today and $row["Avis de l'ONEE"]==NULL)
     {
-        if($row["Avis de l'ONEE"]==NULL)
+        $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis ONEE'";
+        $link->query($req_m);
+    }
+    else {
+        $input1=substr($row["Avis de l'ONEE"], -10);
+        $date2=date('Y-m-d', strtotime($input1. ' + 1 month'));
+        if($date2<=$today and $row["Avis Agence Bassin"]==NULL)
         {
-            $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis ONEE'";
+            $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis Agence Bassin'";
             $link->query($req_m);
         }
-        else {
-            $input1=substr($row["Avis de l'ONEE"], -10);
-            $date2=date('Y-m-d', strtotime($input1. ' + 1 month'));
-            if($date2==$today or $date2<$today)
+        else
+        {
+            $input2=substr($row["Avis Agence Bassin"], -10);
+            $date3=date('Y-m-d', strtotime($input2. ' + 3 months'));
+            if($date3<=$today and $row["Avis MEME"]==NULL)
             {
-                if($row["Avis Agence Bassin"]==NULL)
-                {
-                    $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis Agence Bassin'";
-                    $link->query($req_m);
-                }
-                else {
-                    $input2=substr($row["Avis Agence Bassin"], -10);
-                    $date3=date('Y-m-d', strtotime($input2. ' + 3 months'));
-                    if($date3==$today or $date3<$today )
-                    {
-                        if($row["Avis MEME"]==NULL)
-                        {
-                            $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis MEME'";
-                            $link->query($req_m);
-                        }
-                    }
-                }
+                $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis MEME'";
+                $link->query($req_m);
             }
+
         }
+
+
     }
+
 }
-$sql="SELECT * FROM `alertes_hyd`";
-$res=mysqli_query($link,$sql);
-while ($r= mysqli_fetch_assoc($res)) {
-    $somme=$somme+$r["nbr"];
-}
-echo $somme;
+
 ?>
 
 
--- test.php
+++ test.php
@@ -13,37 +13,32 @@ $result=mysqli_query($link,$sqli);
 while ($row = mysqli_fetch_assoc($result)) {
     $input=substr($row['Depot a la DEREE'], 0, 10);
     $date1=date('Y-m-d', strtotime($input. ' + 15 days'));
-    if($date1==$today or $date1<$today)
+    if($date1<=$today and $row["Avis de l'ONEE"]==NULL)
     {
-        if($row["Avis de l'ONEE"]==NULL)
+        $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis ONEE'";
+        $link->query($req_m);
+    }
+    else {
+        $input1=substr($row["Avis de l'ONEE"], -10);
+        $date2=date('Y-m-d', strtotime($input1. ' + 1 month'));
+        if($date2<=$today and $row["Avis Agence Bassin"]==NULL)
         {
-            echo "onee <br>";
+            $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis Agence Bassin'";
+            $link->query($req_m);
         }
-        else {
-            $input1=substr($row["Avis de l'ONEE"], -10);
-            $date2=date('Y-m-d', strtotime($input1. ' + 1 month'));
-            if($date2==$today or $date2<$today)
+        else
+        {
+            $input2=substr($row["Avis Agence Bassin"], -10);
+            $date3=date('Y-m-d', strtotime($input2. ' + 3 months'));
+            if($date3<=$today and $row["Avis MEME"]==NULL)
             {
-                if($row["Avis Agence Bassin"]==NULL)
-                {
-                    echo "abh <br>";
-                }
-                else {
-                    $input2=substr($row["Avis Agence Bassin"], -10);
-                    $date3=date('Y-m-d', strtotime($input2. ' + 3 months'));
-                    if($date3==$today or $date3<$today )
-                    {
-                        if($row["Avis MEME"]==NULL)
-                        {
-                            echo "meme <br>";
-                        }
-                    }
-
-                }
+                $req_m = "update alertes_hyd set nbr = nbr +1 where quoi = 'Avis MEME'";
+                $link->query($req_m);
             }
 
-
         }
+
+
     }
 
 }
